99c64ff8b288509d2990d9148fc48d7db5c5f90d
Brian Sunbury <bsunbury29@gmail.com>
2022-02-09 14:24:01 -0700

    working with resolutions

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -7,6 +7,7 @@
 // You can delete this file if you're not using it
 
 import * as React from "react"
+import { useState, useEffect } from "react"
 import Amplify, { Auth } from "aws-amplify"
 import { Authenticator } from "@aws-amplify/ui-react"
 import awsExports from "./src/aws-exports"
@@ -15,31 +16,21 @@ import "@aws-amplify/ui-react/styles.css"
 Amplify.configure(awsExports)
 
 // https://www.gatsbyjs.com/docs/reference/config-files/gatsby-browser/#wrapPageElement
-const wrapRootElement = ({ element }) => (
-  <div>
-    <Top />
-    <Authenticator>{element}</Authenticator>
-  </div>
-)
+const wrapRootElement = ({ element }) => <Root element={element} />
 
-const Top = () => {
-  async function signIn() {
-    try {
-      const response = await Auth.signIn("username", "password")
-      console.log("signIn", { response })
-    } catch (e) {
-      console.log("signIn", { e })
-    }
-  }
+const Root = ({ element }) => {
+  const [state, setState] = useState(false)
 
-  return (
-    <div>
-      <div>wrapRootElement</div>
-      <button type="button" onClick={signIn}>
-        Sign In
-      </button>
-    </div>
-  )
+  useEffect(() => {
+    /*
+      Having another issue related to the amplify's Router commponent
+      logging out first thing seems to fix the issue enough so I can test the xstate issue
+    */
+    Auth.signOut()
+    setState(true)
+  }, [])
+
+  return !state ? <div>Loading...</div> : <>{element}</>
 }
 
 export { wrapRootElement }
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,5 +1,6 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import { Authenticator } from "@aws-amplify/ui-react"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
@@ -10,6 +11,7 @@ const SecondPage = () => (
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
+    <Authenticator />
   </Layout>
 )
 
